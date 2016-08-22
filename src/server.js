e3e1df45785fde580a1dadcf2d5c8029064528b9
Melissa Smith <melismae@gmail.com>
2016-08-21 22:58:55 -0700

    nytimes upshot scraper, trying to figure out request referal workaround

--- src/server.js
+++ src/server.js
@@ -2,13 +2,16 @@ import express from 'express';
 import http from 'http';
 import { dbConnection, insertManyDocs, collections } from '../database/connections.js';
 import { fiveThirtyEight } from './scrapers/five-thirty-eight';
+import { nyTimesUpshot } from './scrapers/nytimes-upshot';
+
 import { PRODUCTION_PORT, DEV_PORT, ONE_MINUTE, ONE_HOUR } from '../config/constants';
 
 const CRAWL_INTERVAL = (11 * ONE_HOUR) + (17 * ONE_MINUTE);
 const production = process.env.NODE_ENV === 'production';
 const port = production ? PRODUCTION_PORT : DEV_PORT;
 const scrapers = [
-    fiveThirtyEight
+    // fiveThirtyEight,
+    nyTimesUpshot
 ];
 
 class Application {
@@ -21,7 +24,7 @@ class Application {
         }))
         .then((responses) => {
             // connect and write responses to the db
-            dbConnection(collections.predictionInfo, 'insert', responses);
+            // dbConnection(collections.predictionInfo, 'insert', responses);
             // start the server
             this.startAppServer();
         })
