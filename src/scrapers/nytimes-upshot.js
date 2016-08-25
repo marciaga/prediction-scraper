37ab8482ff5027b33b743db89c2c20c45ecf4111
Melissa Smith <melismae@gmail.com>
2016-08-25 15:29:58 -0700

    fix tmp dir

--- src/scrapers/nytimes-upshot.js
+++ src/scrapers/nytimes-upshot.js
@@ -6,9 +6,9 @@ const fs = require('fs');
 import validateDoc from '../services/validations';
 import { USER_AGENT } from '../../config/constants';
 
-let nightmare = new Nightmare();
+let nightmare = Nightmare();
 
-const tempPath = '../../tmp/';
+const tempPath = './tmp/index.html';
 
 const config = {
     url: 'http://www.bing.com',
@@ -23,9 +23,8 @@ const config = {
 const url = 'http://www.nytimes.com/interactive/2016/upshot/presidential-polls-forecast.html';
 
 // empty out the tmp directory
-rimraf(`${tempPath}*`, function(err) {
+rimraf(tempPath, function(err) {
     if (err) {
-
         console.log(err);
     }
 });
@@ -60,13 +59,12 @@ export const nyTimesUpshot = function() {
         .wait(config.reasonableInterval)
         .click(config.linkSelector)
         .wait(config.reasonableInterval)
-        .html(`${tempPath}index.html`, 'HTMLOnly')
+        .html(tempPath, 'HTMLOnly')
         .wait(10000)
         .end()
         .then(() => {
             // read the index.html page from the fs
-            const file = fs.readFileSync(`${tempPath}index.html`, 'utf8');
-            console.log(file);
+            const file = fs.readFileSync(tempPath, 'utf8');
             // load it into cheerio and parse it
             const $ = cheerio.load(file);
 
@@ -84,7 +82,6 @@ export const nyTimesUpshot = function() {
             if (!validateDoc(doc)) {
                 return {};
             }
-            console.log(doc);
             return doc;
         }))
         .catch((err) => {
